144c68aa2f3fa15609d09b18c45dbc2e22a98121
Sch <junhg0211@gmail.com>
2026-04-29 22:43:34 +0900

    Add styling and navigation to layout; create game page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,76 @@
 </script>
 
 <svelte:head>
+	<style>
+		body {
+			margin: 0;
+			font-family: system-ui, sans-serif;
+			overflow: hidden;
+		}
+
+		a {
+			color: inherit;
+			text-decoration: none;
+		}
+
+		a:hover {
+			text-decoration: underline;
+		}
+	</style>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children()}
+<div class="container">
+	<div class="navigation">
+		<div class="logo">Grav</div>
+		<div class="links">
+			<div class="link"><a href="/play">플레이</a></div>
+		</div>
+		<div class="footbar">Hello, world!</div>
+	</div>
+	<div class="content">
+		{@render children()}
+	</div>
+</div>
+
+<style>
+	.container {
+		display: flex;
+		flex-direction: row;
+	}
+
+	.navigation {
+		height: 60px;
+		background-color: #333;
+		color: white;
+		padding: 20px 20px;
+		display: flex;
+		flex-direction: column;
+		height: 100vh;
+		position: sticky;
+		box-sizing: border-box;
+	}
+
+	.content {
+		flex: 1;
+		max-height: 100vh;
+		overflow-y: auto;
+	}
+
+	.logo {
+		font-size: 1.5em;
+		font-weight: bold;
+	}
+
+	.links {
+		margin-top: 20px;
+		display: flex;
+		flex-direction: column;
+		gap: 10px;
+		flex: 1;
+	}
+
+	.link {
+		font-size: 1.2em;
+	}
+</style>
---
+++ src/routes/game/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Gravjulien from '$lib/components/Gravjulien.svelte';
+
+	const gravjulien = {
+		size: 8,
+		grasooshas: [
+			{ x: 1, y: 1, color: '#ffce00', username: '북극치타' },
+			{ x: 2, y: 2, color: '#000000', username: 'zasoque' },
+			{ x: 3, y: 3, color: '#ff0000', username: 'もにゃご' },
+			{ x: 4, y: 4, color: '#0000ff', username: '什麼' },
+			{ x: 5, y: 5, color: '#ffffff', username: 'a' }
+		],
+		last: { x: 4, y: 3 }
+	};
+</script>
+
+<Gravjulien {gravjulien} />
